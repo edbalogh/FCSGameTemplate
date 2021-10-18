20c2e281322219a9beb464d8380576141583a69a
Ed Balogh <ed.balogh@gmail.com>
2021-10-18 13:19:06 +0000

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,67 @@
+
+// grab the canvas object created in index.html
+const canvas = document.getElementById("gameCanvas");
+
+// pull a reference to the 2d context from that canvas
+const ctx = canvas.getContext("2d");
+
+// set the canvas width and height
+canvas.width = 800;
+canvas.height = 600;
+
+// initialize variables
+let x = 10;
+let y = 10
+let lastTime = 0;
+let speed = 0;
+
+// set starting color of square
+ctx.fillStyle = "red";
+
+// start animation/game loop
+requestAnimationFrame(gameLoop);
+
+function gameLoop(timestamp) {
+  // get the amount of time that has passed since last update
+  const deltaTime = timestamp - lastTime;
+
+  // detect collision between square and right side of screen
+  if (x >= 290) {
+    speed = 0;
+    x = 289;
+  }
+
+  // detect collision between square and left side of screen
+  if (x < 0) {
+    speed = 0;
+    x = 1;
+  }
+  
+  // update x value based on speed and time passed
+  x += speed / deltaTime;
+  lastTime = timestamp;
+
+  // clear screen
+  ctx.clearRect(0, 0, 800, 600)
+
+  // draw square in current position
+  ctx.fillRect(x, y, 10, 10);
+
+  // next frame in loop
+  requestAnimationFrame(gameLoop);
+}
+
+
+// adding event listeners
+addEventListener('keydown', (event) => {
+  // move right/left based on arrow keys
+  if(event.keyCode === 39) speed += 10;
+  if(event.keyCode === 37) speed -= 10;
+  // alert(`you pressed keyCode ${event.keyCode}`);
+});
+
+addEventListener('keyup', (event) => {
+  // stop x movement on any key up
+  speed = 0;
+})
+
